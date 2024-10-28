d6c8a591371a55a882ee122f3e1b1a6ff606ae80
cqlyj <luoyingjie0721@gmail.com>
2024-10-28 22:56:52 +0800

    Refactor MinimalAccountTest contract to include testEntryPointCanExecuteCommands function

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -86,7 +86,8 @@ contract MinimalAccountTest is Test {
         PackedUserOperation memory packedUserOp = sendPackedUserOp
             .generateSignedUserOperation(
                 executeCallData,
-                helperConfig.getConfig()
+                helperConfig.getConfig(),
+                address(minimalAccount)
             );
 
         bytes32 userOperationHash = IEntryPoint(
@@ -121,7 +122,8 @@ contract MinimalAccountTest is Test {
         PackedUserOperation memory packedUserOp = sendPackedUserOp
             .generateSignedUserOperation(
                 executeCallData,
-                helperConfig.getConfig()
+                helperConfig.getConfig(),
+                address(minimalAccount)
             );
 
         bytes32 userOperationHash = IEntryPoint(
@@ -140,5 +142,45 @@ contract MinimalAccountTest is Test {
         assertEq(validationData, 0);
     }
 
-    function testEntryPointCanExecuteCommands() public {}
+    function testEntryPointCanExecuteCommands() public {
+        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(
+            ERC20Mock.mint.selector,
+            address(minimalAccount),
+            AMOUNT
+        );
+
+        bytes memory executeCallData = abi.encodeWithSelector(
+            MinimalAccount.execute.selector,
+            dest,
+            value,
+            functionData
+        );
+
+        PackedUserOperation memory packedUserOp = sendPackedUserOp
+            .generateSignedUserOperation(
+                executeCallData,
+                helperConfig.getConfig(),
+                address(minimalAccount)
+            );
+
+        // bytes32 userOperationHash = IEntryPoint(
+        //     helperConfig.getConfig().entryPoint
+        // ).getUserOpHash(packedUserOp);
+
+        vm.deal(address(minimalAccount), 1e18);
+
+        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+        ops[0] = packedUserOp;
+
+        vm.prank(randomUser);
+        IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(
+            ops,
+            payable(randomUser)
+        );
+
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
 }
